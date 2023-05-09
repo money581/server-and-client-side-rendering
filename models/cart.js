ab229cf5e4096a15643b53775941d4b390eb6f35
Aman <amanagrawal581@gmail.com>
2023-05-09 22:07:43 +0530

    dynamic routes part 3 edit and delete btn

--- models/cart.js
+++ models/cart.js
@@ -18,24 +18,53 @@ module.exports = class Cart {
             }
             //analyza the cart  find exixting product
             const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-            const existingProduct=cart.products[existingProductIndex];
+            const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
             //add new product and update quantity
             if (existingProduct) {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [ ...cart.products];
-                cart.products[existingProductIndex]=updatedProduct;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
             } else {
                 updatedProduct = { id: id, qty: 1 };
-                cart.products = [ ...cart.products ,updatedProduct];
+                cart.products = [...cart.products, updatedProduct];
             }
             cart.totalPrice = cart.totalPrice + +productPrice;
-            fs.writeFile(p,JSON.stringify(cart),(err)=>{
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
                 console.log(err);
             })
-           
+
         });
 
     }
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return;
+            }
+            const updatedCart = { ...JSON.parse(fileContent) };
+            const product = updatedCart.products.find(prod => prod.id === id);
+            if(!product){
+                return;
+            }
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+                console.log(err);
+            })
+        })
+    }
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if (err) {
+                cb(null)
+            } else {
+                cb(cart);
+            }
+
+        });
+    };
 };
