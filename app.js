b2bf24829609422a73e22e2160022b6271febbf3
Aman <amanagrawal581@gmail.com>
2023-05-15 19:52:35 +0530

    jst add sequelize setup in express js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db=require('./util/database');
+const sequelize=require('./util/database');
 
 const app = express();
 
@@ -34,5 +34,12 @@ app.use(shopRoutes);
 // })
 
 app.use(errorController.get404);
+sequelize.sync().then(result=>{
+    
+    app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+});
 
-app.listen(3000);
+
--- util/database.js
+++ util/database.js
@@ -1,9 +1,3 @@
-const mysql=require('mysql2');
-const pool=mysql.createPool({
-host: 'localhost',
-user: 'root',
-database: 'node-complete',
-password: 'Money$aman@12345'
-});
-
-module.exports=pool.promise();
+const Sequelize=require('sequelize');
+const sequelize=new Sequelize('node-complete','root','Money$aman@12345',{dialect: 'mysql',host:'localhost'});
+module.exports=sequelize;
