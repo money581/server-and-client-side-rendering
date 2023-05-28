1abe52a30ee392fcf3e1122cdfe2744ba9d176ff
Aman <amanagrawal581@gmail.com>
2023-05-28 19:19:45 +0530

    in express js,Dsign the User nd Product rlationsip

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize=require('./util/database');
-
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,6 +19,14 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => {
+        console.log(err);
+    })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,12 +43,27 @@ app.use(shopRoutes);
 // })
 
 app.use(errorController.get404);
-sequelize.sync().then(result=>{
-    
-    app.listen(3000);
-})
-.catch(err=>{
-    console.log(err);
-});
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+// sequelize.sync({force:true}).then(result=>{
+sequelize.sync()
+    .then(result => {
+        return User.findByPk(1);
+
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'aman', email: 'amanagrawal581@gmail.com' });
+        }
+        return user;
+    })
+    .then(user => {
+
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
- Product.create({
-  title:title,
-  price:price,
-  imageUrl:imageUrl,
-  description:description
- }).then(result=>{
+  req.user.createProduct({
+    title:title,
+    price:price,
+    imageUrl:imageUrl,
+    description:description,
+    
+   })
+ .then(result=>{
   console.log("created product")
   res.redirect('/admin/products');
  }).catch(err=>{
@@ -33,7 +35,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId ).then(product => {
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findByPk(prodId )
+  .then(products => {
+    const product=products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -74,7 +79,8 @@ Product.findByPk(prodId).then(product=>{
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
